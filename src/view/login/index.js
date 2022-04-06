9268a889a45b5d3232389f35ac7044ca95d4511f
Luis Rubio <rubioluisgustavo@outlook.com>
2022-04-06 14:22:46 -0300

    first commit

---
+++ src/view/login/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './login.css';
+
+function Login() {
+    return (
+        <div className='login-content d-flex align-items-center'>
+            <main className="form-signin mx-auto">
+                <form>
+                    <h1 className='h1h1 text-center text-white'>Login</h1>
+
+                    <input type="email" className="form-control my-2" id="floatingInput" placeholder="react@gmail.com"/>
+                    <input type="password" className="form-control my-2" id="floatingPassword" placeholder="******" />
+
+                    <button className="w-100 btn btn-lg btn-login" type="submit">Entrar</button>
+                    <div className='opcoes-login'>
+                        <a href="" className='mx-2'>Recuperar Senha</a>
+                        <a href="" className='mx-2'>Cadastre-se</a>
+                    </div>
+                    <p className="text-center mt-5 mb-3 ano">&copy; 1996-2022 - por Luis Rubio</p>
+                </form>
+            </main>
+        </div>
+    )
+}
+
+export default Login;
