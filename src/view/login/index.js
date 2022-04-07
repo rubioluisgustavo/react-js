c5c7026ba90e609580dac8eaab60b3ce1ca4bf96
Luis Rubio <rubioluisgustavo@outlook.com>
2022-04-07 20:18:57 -0300

    cadastro de usuário

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -11,23 +11,23 @@ function Login() {
 
     function logar() {
 
-        firebase.auth().signInWithEmailAndPassword(email, senha).then
-        
-        (resultado => {setMsgTipo('sucesso');
-        }).catch(erro => {setMsgTipo('erro');})
-    
+        firebase.auth().signInWithEmailAndPassword(email, senha)
+
+            .then(resultado => { setMsgTipo('sucesso'); })
+            .catch(erro => { setMsgTipo('erro'); })
+
     }
 
     return (
         <div className='login-content d-flex align-items-center'>
-            <main className="form-signin mx-auto">
+            <main className="form-signin text-center mx-auto">
                 <form>
                     <h1 className='h1h1 text-center text-white'>Login</h1>
 
                     <input onChange={(e) => setEmail(e.target.value)} type="email" className="email form-control my-2" id="email" placeholder="react@gmail.com" />
                     <input onChange={(e) => setSenha(e.target.value)} type="password" className="senha form-control my-2" id="senha" placeholder="******" />
 
-                    <button onClick={ logar } className="w-100 btn btn-lg btn-login" type="button">Entrar</button>
+                    <button onClick={logar} className="w-100 btn btn-lg btn-login" type="button">Entrar</button>
 
                     <div className='text-center msg-login my-4'>
                         {msgTipo === 'sucesso' && <span>Você está <strong>conectado!</strong> &#128512;</span>}
--- src/view/usuario-novo/index.js
+++ src/view/usuario-novo/index.js
@@ -4,16 +4,40 @@ import firebase from '../../config/./firebase';
 import 'firebase/auth';
 
 function NovoUsuario() {
+
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+    const [msgTipo, setMsgTipo] = useState();
+    const [msg, setMsg] = useState();
+
+    function cadastrar() {
+        setMsgTipo(null);
+
+        if (!email || !senha) {
+            setMsgTipo('erro')
+            setMsg('Você precisa informar o email e senha para fazer o cadastro!');
+            return;
+        }
+
+        firebase.auth().createUserWithEmailAndPassword(email, senha)
+
+            .then(resultado => { setMsgTipo('sucesso') })
+            .catch(erro => { setMsgTipo('erro')})
+    }
+
     return (
-        <div className='form-cadastro'>
-            <form className='text-center form-login mx-auto mt-5'>
-                <h1 className='h3 mb-3 text-black font-weight-bold'>Cadastro</h1>
-                <input type="email" className='form-control my-2' placeholder='Email' />
-                <input type="password" className='form-control my-2' placeholder='Senha' />
-                <button type='button' className='btn btn-lg btn-block mt-3 mb-5 btn-cadastro'>Cadastrar</button>
-                <div className='text-center msg-login my-4'>
-                    {msgTipo === 'sucesso' && <span>Você está <strong>conectado!</strong> &#128512;</span>}
-                    {msgTipo === 'erro' && <span>E-mail ou senha <strong>inválidos!</strong> &#128552;</span>}
+        <div className='form-cadastro d-flex align-items-center'>
+            <form className='text-center mx-auto'>
+                <h1 className='h1h1 text-center text-white'>Cadastro</h1>
+
+                <input onChange={(e) => setEmail(e.target.value)} type="email" className="email form-control my-2" id="email" placeholder="react@gmail.com" />
+                <input onChange={(e) => setSenha(e.target.value)} type="password" className="senha form-control my-2" id="senha" placeholder="******" />
+
+                <button onClick={cadastrar} type='button' className='btn btn-lg btn-block mt-3 mb-3 btn-cadastro'>Cadastrar</button>
+
+                <div className='text-center msg-cad'>
+                    {msgTipo === 'sucesso' && <span>Cadastrado com sucesso! &#128512;</span>}
+                    {msgTipo === 'erro' && <span>E-mail ou senha inválidos. &#128552;</span>}
                 </div>
             </form>
         </div>
